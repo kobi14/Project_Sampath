56104ad05e999c7b0567ff1ae9a47f9abe9dfbab
kobi14 <2011chava@gmail.com>
2017-07-18 09:58:34 +0530

    add slider

--- re_1/engine1/script.js
+++ re_1/engine1/script.js
@@ -18,4 +18,4 @@ function ws_blur(r,p,c){var h=jQuery;var o=h(this);var n=h("<div>").addClass("ws
 // Obfuscated by Javascript Obfuscator
 // http://javascript-source.com
 //***********************************************
-jQuery("#wowslider-container1").wowSlider({effect:"blur",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
+jQuery("#wowslider-container1").wowSlider({effect:"blur",prev:"",next:"",duration:20*100,delay:20*100,width:640,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
